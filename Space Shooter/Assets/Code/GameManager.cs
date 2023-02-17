5f46eca026bcc824a7cae4e24d4dddbb01c798c2
hahyoungkim <hyk323@nyu.edu>
2023-02-17 17:05:17 -0500

    hahyoung print test

--- Space Shooter/Assets/Code/GameManager.cs
+++ Space Shooter/Assets/Code/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("hahyoung print");
     }
 
     // Update is called once per frame
